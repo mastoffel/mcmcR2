31bc41cca7b6430fd86e9e63a46540776721293a
Martin Stoffel <martin.adam.stoffel@gmail.com>
2018-06-05 14:26:25 +0200

    added conditional R2

--- R/R2mcmc.R
+++ R/R2mcmc.R
@@ -24,7 +24,8 @@
 #'           family=c("gaussian"),ginverse=list(tip_label=inv_phylo),prior=prior,
 #'           data=seal_data,nitt=1100,burnin=100,thin=10)
 #'
-#' out <- R2mcmc(mod_gen)
+#' out <- R2mcmc(mod_gen, type = "marginal")
+#' out <- R2mcmc(mod_gen, type = "conditional")
 #' out$partR2
 #'
 #'
@@ -33,11 +34,11 @@
 #'
 
 R2mcmc <- function(mod, type = "marginal", family = "gaussian"){
-    if (type != "marginal") stop("At the moment, there is just the marginal R2")
+   # if (type != "marginal") stop("At the moment, there is just the marginal R2")
     if (family != "gaussian") stop("At the moment just gaussian errors are supported")
     # Shinichis answer on Researchgate
     mVarF <- var(as.vector(apply(mod$Sol,2,mean) %*% t(as.matrix(mod$X))))  #t(as.matrix(mod$X))))
-    R2 <- mVarF/(mVarF+sum(apply(mod$VCV,2,mean)))
+    # R2 <- mVarF/(mVarF+sum(apply(mod$VCV,2,mean)))
 
     # alternative with crebile intervals
     mcmc_chain_length <- nrow(mod$VCV)
@@ -46,15 +47,21 @@ R2mcmc <- function(mod, type = "marginal", family = "gaussian"){
     vmVarF <- vapply(1:mcmc_chain_length, function(x) out <- var(as.vector(mod$Sol[x,] %*% t(as.matrix(mod$X)))), #t(as.matrix(mod$X))))
         FUN.VALUE = numeric(length(mcmc_chain_length)))
 
-    R2m<-vmVarF/(vmVarF+ rowSums(mod$VCV)) # include here all random effects plus errors
+    if (type == "marginal"){
+        R2m <- vmVarF/(vmVarF+ rowSums(mod$VCV)) # include here all random effects plus errors
+    } else if (type == "conditional"){
+        # Delete last column in VCV which contains residuals (observational level variance)
+        R2m <- (vmVarF + rowSums(mod$VCV[, -ncol(mod$VCV), drop=FALSE]))/(vmVarF+ rowSums(mod$VCV))
+
+    }
     outR2m <- R2m
     class(R2m) <- "mcmc"
-    R2m<-vmVarF/(vmVarF+mod$VCV[,1]+mod$VCV[,2])
 
+    # R2m<-vmVarF/(vmVarF+mod$VCV[,1]+mod$VCV[,2])
     #mean(R2m)
     #posterior.mode(R2m)
     #data.frame(HPDinterval(R2m), row.names = NULL)
 
-    out <- list("partR2" = data.frame("meanR2" = mean(R2m),"medianR2" = median(R2m), "modeR2" =  posterior.mode(R2m), data.frame(HPDinterval(R2m)), row.names = NULL),
-        "R2_chain" = outR2m)
+    out <- list("partR2" = data.frame("meanR2" = mean(R2m),"medianR2" = median(R2m), "modeR2" = posterior.mode(R2m), data.frame(HPDinterval(R2m)), row.names = NULL),
+        "R2m_chain" = outR2m)
 }
